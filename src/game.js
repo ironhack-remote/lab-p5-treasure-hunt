ed09cc81ea8240da8d6148a7227b3e49a6b24b73
Marcel Niebylski <mnblski@gmail.com>
2020-10-29 17:55:24 +0000

    Iteration 5 and 6 left + cslow down the movement

--- src/game.js
+++ src/game.js
@@ -1,7 +1,28 @@
 class Game {
+  constructor() {
+    this.player = new Player(0, 0);
+  }
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (let i = 0; i < 11; i++) {
+      let side = SQUARE_SIDE * i;
+      line(0, side, WIDTH, side);
+      line(side, 0, side, WIDTH);
+    }
+  }
+
+  keyPressed() {
+    if (keyCode === 37) {
+      this.player.moveLeft();
+    }
+    if (keyCode === 38) {
+      this.player.moveUp();
+    }
+    if (keyCode === 39) {
+      this.player.moveRight();
+    }
+    if (keyCode === 40) {
+      this.player.moveDown();
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 const game = new Game();
 
+function preload() {
+  pic = loadImage("/lab-p5-treasure-hunt/assets/character-down.png");
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  game.player.draw();
+  game.keyPressed();
 }
